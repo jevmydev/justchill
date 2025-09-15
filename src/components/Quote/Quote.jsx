81376c581573dba21198875edaa3ac30ffaf8ae4
Jeremy <jeremy664k@gmail.com>
2025-09-15 17:36:49 -0300

    Update quote with quotation marks

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -9,12 +9,12 @@ export function Quote() {
             <div className="flex flex-col gap-8 py-24">
                 <h2 className="text-3xl font-semibold text-green-100">Frase del día</h2>
                 <blockquote className="text-4xl italic font-medium text-green-300 p-8 rounded-lg border-l-4 border-green-500">
-                    <h3>{quote}</h3>
+                    <h3>"{quote}"</h3>
                 </blockquote>
 
                 <h2 className="text-3xl font-semibold text-green-100">Intención del día</h2>
                 <blockquote className="text-4xl italic font-medium text-green-300 p-8 rounded-lg border-l-4 border-green-500">
-                    <h3>{intention}</h3>
+                    <h3>"{intention}"</h3>
                 </blockquote>
             </div>
         </section>
